cf209f7b33e9983b54b495b2a767993b6d5d8e98
Steven Deobald <steven@deobald.ca>
2021-03-06 20:03:25 -0600

    Added fixtures

--- test/joplin/crux/database_test.clj
+++ test/joplin/crux/database_test.clj
@@ -12,9 +12,28 @@
 (defn crux-conf []
   (-> config :databases :cx-dev :conf))
 
-(defn destroy-node []
+(defn destroy-node! []
   (reset! sut/crux-node nil)
   (sut/get-node (crux-conf)))
+
+(defn destroy-fixture-file-copies! []
+  (io/delete-file "test-resources/joplin/migrators/crux/20210306000000_second_migrator.clj"
+                  :ignore-missing-file))
+
+(defn copy-fixture-file! []
+  (io/copy (io/file "test-resources/fixtures/20210306000000_second_migrator.clj")
+           (io/file "test-resources/joplin/migrators/crux/20210306000000_second_migrator.clj")))
+
+(defn with-empty-node [f]
+  (destroy-node!)
+  (f))
+
+(defn with-fixture-files-cleanup [f]
+  (destroy-fixture-file-copies!)
+  (f)
+  (destroy-fixture-file-copies!))
+
+(use-fixtures :each with-empty-node with-fixture-files-cleanup)
 
 (defn query-migrations []
   (x/q (x/db (sut/get-node (crux-conf)))
@@ -29,22 +48,19 @@
 (deftest adding-migrations
   (testing "adds one migration"
     (repl/migrate config :dev)
-    (is (= 1 (-> (query-migrations)
-                 (count))))))
+    (is (= 1 (-> (query-migrations) (count))))))
 
 (deftest removing-migrations
   (testing "removes one migration from 'now'"
     (repl/migrate config :dev)
     (repl/rollback config :dev :cx-dev 1)
-    (is (= 0 (-> (query-migrations)
-                 (count)))))
+    (is (= 0 (-> (query-migrations) (count)))))
 
   (testing "removing a migration does NOT remove it from valid-time history"
     (repl/migrate config :dev)
     (let [between (java.util.Date.)
           _ (Thread/sleep 500)]
-      (is (= 1 (-> (query-migrations)
-                   (count))))
+      (is (= 1 (-> (query-migrations) (count))))
       (repl/rollback config :dev :cx-dev 1)
       (is (= 1 (-> (x/q (x/db (sut/get-node (crux-conf)) between)
                         '{:find [e]
@@ -53,42 +69,33 @@
 
 (deftest seeding
   (testing "adds seed data"
-    (destroy-node)
     (repl/migrate config :dev)
     (repl/seed config :dev)
-    (is (= 3 (-> (query-seeds)
-                 (count))))))
+    (is (= 3 (-> (query-seeds) (count))))))
 
 (deftest resetting
   (testing "resetting the node removes and replaces old migrations"
-    (destroy-node)
+    (destroy-node!)
     (repl/migrate config :dev)
     (repl/seed config :dev)
     (repl/reset config :dev :cx-dev)
-    (is (= 1 (-> (query-migrations)
-                 (count)))))
+    (is (= 1 (-> (query-migrations) (count)))))
 
   (testing "resetting the node does NOT remove old seeds"
-    (destroy-node)
+    (destroy-node!)
     (repl/migrate config :dev)
     (repl/seed config :dev)
     (repl/reset config :dev :cx-dev)
-    (is (= 6 (-> (query-seeds)
-                 (count))))))
+    (is (= 6 (-> (query-seeds) (count))))))
 
 (deftest pending
   (testing "pending knows how many migrations are left to run"
-    (destroy-node)
     (repl/migrate config :dev)
-    (io/copy (io/file "test-resources/fixtures/20210306000000_second_migrator.clj")
-             (io/file "test-resources/joplin/migrators/crux/20210306000000_second_migrator.clj"))
+    (copy-fixture-file!)
     (is (= "Pending migrations (20210306000000-second-migrator)\n"
-           (with-out-str (repl/pending config :dev :cx-dev))))
-    (io/delete-file "test-resources/joplin/migrators/crux/20210306000000_second_migrator.clj"
-                    :ignore-missing-file)))
+           (with-out-str (repl/pending config :dev :cx-dev))))))
 
 ;; TODO:
-;; - add fixtures
 ;; (repl/create)
 ;; - use transaction fns
 ;; - extract a submit+await+entity+exception fn
