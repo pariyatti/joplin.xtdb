a5f6cb78cada286035c248a4dccc3748c37315e5
Steven Deobald <steven@deobald.ca>
2021-03-13 23:29:03 -0600

    Trying another weird hack (wrapping joplin.alias)

---
+++ src/joplin/crux/alias.clj
@@ -0,0 +1,13 @@
+(ns joplin.crux.alias
+  (:require [joplin.alias]
+            [joplin.repl]))
+
+(def migrate joplin.alias/migrate)
+(def seed joplin.alias/seed)
+(def rollback joplin.alias/rollback)
+(def reset joplin.alias/reset)
+(def pending joplin.alias/pending)
+(def create joplin.alias/create)
+
+(ns joplin.repl
+  (:require [joplin.crux.database]))
