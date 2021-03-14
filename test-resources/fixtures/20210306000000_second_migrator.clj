185017ef5404f03c59e360d4b102fb15d33bb5b1
Steven Deobald <steven@deobald.ca>
2021-03-13 18:26:10 -0600

    Make use of 'transact!' in the test fixture

--- test-resources/fixtures/20210306000000_second_migrator.clj
+++ test-resources/fixtures/20210306000000_second_migrator.clj
@@ -7,16 +7,12 @@
 
 (defn up [db]
   (let [node (d/get-node (:conf db))
-        tx (x/submit-tx node [[:crux.tx/put {:crux.db/id id
-                                             :schema/id id
-                                             :schema/created-at (t/now)}]])
-        _ (x/await-tx node tx)]
-    ;; it might be wise for real migrators to assert the migration was
-    ;; successfully saved like this:
-    (when-not (x/entity (x/db node) id)
-      (throw (Exception. (format "Migrator '%s' failed to apply." id))))))
+        txs [[:crux.tx/put {:crux.db/id id
+                            :schema/id id
+                            :schema/created-at (t/now)}]]]
+    (d/transact! node txs (format "Migrator '%s' failed to apply." id))))
 
 (defn down [db]
   (let [node (d/get-node (:conf db))
-        tx (x/submit-tx node [[:crux.tx/delete id]])
-        _ (x/await-tx node tx)]))
+        txs [[:crux.tx/delete id]]]
+    (d/transact! node txs (format "Rollback '%s' failed to apply." id))))
