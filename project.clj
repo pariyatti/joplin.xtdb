55db443c49c4d96aafbeab962daa08c348ec999e
Steven Deobald <steven@deobald.ca>
2021-03-16 17:04:30 -0600

    Preparing for 0.0.1 Clojars release

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject joplin.crux "0.0.1-SNAPSHOT"
+(defproject org.pariyatti/joplin.crux "0.0.1"
   :description "Crux support for Joplin"
   :url "http://github.com/pariyatti/joplin.crux"
   :scm {:name "git"
         :url "https://github.com/pariyatti/joplin.crux"}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [joplin.core "0.3.11"]
                  [juxt/crux-core "21.02-1.15.0-beta"]]
 
